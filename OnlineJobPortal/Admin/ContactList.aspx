c907ab2251b520671316bce6a0344a02a32b2f9c
Vaibhav <vk442002@gmail.com>
2022-02-01 19:33:40 +0530

    feat: add contact list page. add delete page and bind data with grid view

---
+++ OnlineJobPortal/Admin/ContactList.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/AdminMaster.Master" AutoEventWireup="true" CodeBehind="ContactList.aspx.cs" Inherits="OnlineJobPortal.Admin.ContactList" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <div class="container pt-4 pb-4">
+    <div>
+        <asp:Label ID="lblMsg" runat="server"></asp:Label>
+    </div>
+    <div>
+    <h3 class="text-center">Contact List/Details</h3>
+        <div class="row mb-3 pt-sm-3">
+            <div class="col-md-12">
+                <asp:GridView ID="GridView1" runat="server" CssClass="table table-hover table-bordered" EmptyDataText="No record to display..!"
+                              AutoGenerateColumns="False" AllowPaging="True" PageSize="5" OnPageIndexChanging="GridView1_OnPageIndexChanging" 
+                              DataKeyNames="ContactId" OnRowDeleting="GridView1_OnRowDeleting">
+                    <Columns>
+                        <asp:BoundField DataField="Sr.No" HeaderText="Sr.No">
+                        <ItemStyle HorizontalAlign="Center" />
+                        </asp:BoundField>
+                        
+                        <asp:BoundField DataField="Name" HeaderText="User Name">
+                            <ItemStyle HorizontalAlign="Center" />
+                        </asp:BoundField>
+                                                
+                        <asp:BoundField DataField="Email" HeaderText="Email">
+                            <ItemStyle HorizontalAlign="Center" />
+                        </asp:BoundField>
+
+                        
+                        <asp:BoundField DataField="Subject" HeaderText="Subject">
+                            <ItemStyle HorizontalAlign="Center" />
+                        </asp:BoundField>
+
+                        
+                        <asp:BoundField DataField="Message" HeaderText="Message">
+                            <ItemStyle HorizontalAlign="Center" />
+                        </asp:BoundField>
+
+                        
+                                             
+                        <asp:CommandField CausesValidation="False" HeaderText="Delete" ShowDeleteButton="True"
+                                          DeleteImageUrl="../assets/img/icon/trashIcon.png" ButtonType="Image">
+                            <ControlStyle Height="25px" Width="25px"></ControlStyle>
+                            <ItemStyle HorizontalAlign="Center"></ItemStyle>
+                        </asp:CommandField>
+                   </Columns>
+                    <HeaderStyle BackColor="#7200cf" ForeColor="White"></HeaderStyle>
+                </asp:GridView>
+            </div>
+        </div>
+    </div></div>
+</asp:Content>
--- OnlineJobPortal/Admin/ContactList.aspx.cs
+++ OnlineJobPortal/Admin/ContactList.aspx.cs
@@ -10,7 +10,7 @@ using System.Web.UI.WebControls;
 
 namespace OnlineJobPortal.Admin
 {
-    public partial class JobList : System.Web.UI.Page
+    public partial class ContactList : System.Web.UI.Page
     {
         SqlConnection con;
         SqlCommand cmd;
@@ -18,9 +18,10 @@ namespace OnlineJobPortal.Admin
 
         string str = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
         string query;
-
-        protected void Page_PreRender(object sender, EventArgs e)
+        protected void Page_Load(object sender, EventArgs e)
         {
+           
+
             if (Session["admin"] == null)
             {
                 Response.Redirect("../User/Login.aspx");
@@ -28,51 +29,44 @@ namespace OnlineJobPortal.Admin
 
             if (!IsPostBack)
             {
-                ShowJob();
+                ShowContact();
             }
 
         }
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            ShowJob();
-        }
 
-        private void ShowJob()
+        private void ShowContact()
         {
             query = string.Empty;
             con = new SqlConnection(str);
-            query = @"Select Row_Number() over(Order by (Select 1)) as [Sr.No], JobId,Title,NoOfPost,Description,Qualification,Experience,
-                        LastDatetoApply,CompanyName,Country,State,CreateDate from Jobs";
+            query = @"Select Row_Number() over(Order by (Select 1)) as [Sr.No], ContactId,Name,Email,Subject,Message from Contact";
             cmd = new SqlCommand(query, con);
             SqlDataAdapter sda = new SqlDataAdapter(cmd);
             dt = new DataTable();
             sda.Fill(dt);
             GridView1.DataSource = dt;
             GridView1.DataBind();
-            //throw new NotImplementedException();
         }
 
         protected void GridView1_OnPageIndexChanging(object sender, GridViewPageEventArgs e)
         {
             GridView1.PageIndex = e.NewPageIndex;
-            ShowJob();
-
+            ShowContact();
         }
 
         protected void GridView1_OnRowDeleting(object sender, GridViewDeleteEventArgs e)
-        {   
+        {
             try
             {
                 GridViewRow row = GridView1.Rows[e.RowIndex];
-                int jobId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+                int contactId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
                 con = new SqlConnection(str);
-                cmd = new SqlCommand("Delete from Jobs where JobId=@id", con);
-                cmd.Parameters.AddWithValue("@id", jobId);
+                cmd = new SqlCommand("Delete from Contact where ContactId=@id", con);
+                cmd.Parameters.AddWithValue("@id", contactId);
                 con.Open();
                 int r = cmd.ExecuteNonQuery();
                 if (r > 0)
                 {
-                    lblMsg.Text = "Job Delete Successfully!";
+                    lblMsg.Text = "Contact Deleted Successfully!";
                     lblMsg.CssClass = "alert alert-success";
 
                 }
@@ -83,7 +77,7 @@ namespace OnlineJobPortal.Admin
                 }
                 con.Close();
                 GridView1.EditIndex = -1;
-                ShowJob();
+                ShowContact();
             }
             catch (Exception ex)
             {
@@ -91,16 +85,6 @@ namespace OnlineJobPortal.Admin
                 Response.Write("<script>alert('" + ex.Message + "');</script>");
 
             }
-
-        }
-
-        protected void GridView1_OnRowCommand(object sender, GridViewCommandEventArgs e)
-        {
-            if (e.CommandName == "EditJob")
-            {
-                Response.Redirect("NewJob.aspx?id="+e.CommandArgument.ToString());
-            }
-
         }
     }
 }
